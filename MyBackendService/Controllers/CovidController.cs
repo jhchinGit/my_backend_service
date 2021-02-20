76b2abaccb929db8b14e8fcc07e9c80770f99bfd
JHSW <50164113+jhchinGit@users.noreply.github.com>
2021-02-20 22:40:08 +0800

    Update project

---
+++ MyBackendService/Controllers/CovidController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace MyBackendService.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CovidController : ControllerBase
+    {
+        private readonly ILogger<CovidController> _logger;
+
+        public CovidController(ILogger<CovidController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public ActionResult<int> Get()
+        {
+            return Ok(1);
+        }
+    }
+}
---
+++ MyBackendService/Models/DTOs/CovidDailyReport.cs
@@ -0,0 +1,47 @@
+﻿using Newtonsoft.Json;
+using System;
+
+namespace MyBackendService.Models.DTOs
+{
+    public class CovidDailyReport
+    {
+        [JsonProperty("totalCase")]
+        public int TotalCase { get; set; }
+
+        [JsonProperty("newCase")]
+        public int NewCase { get; set; }
+
+        [JsonProperty("newCaseRate")]
+        public decimal NewCaseRate { get; set; }
+
+        [JsonProperty("recovered")]
+        public int Recovered { get; set; }
+
+        [JsonProperty("recoveredNew")]
+        public int RecoveredNew { get; set; }
+
+        [JsonProperty("recoveredNew")]
+        public decimal RecoveredRate { get; set; }
+
+        [JsonProperty("activeCase")]
+        public int ActiveCase { get; set; }
+
+        [JsonProperty("todayActiveCase")]
+        public int TodayActiveCase { get; set; }
+
+        [JsonProperty("activeRate")]
+        public decimal ActiveRate { get; set; }
+
+        [JsonProperty("death")]
+        public int Death { get; set; }
+
+        [JsonProperty("todayDeathCase")]
+        public int TodayDeathCase { get; set; }
+
+        [JsonProperty("deathRate")]
+        public decimal DeathRate { get; set; }
+
+        [JsonProperty("reportedDate")]
+        public DateTime ReportedDate { get; set; }
+    }
+}
